d0d27b6a5a7b9b6d5fb887fb3eb792fc9b3d4819
xiaohao <19929238507@163.com>
2021-11-10 15:13:31 +0800

    新增：banner接口

--- LeastMall/routers/backendRouter.go
+++ LeastMall/routers/backendRouter.go
@@ -19,13 +19,21 @@ func init() {
 		beego.NSRouter("/administrator/goedit", &backend.AdministratorController{}, "post:GoEdit"),
 		beego.NSRouter("/administrator/delete", &backend.AdministratorController{}, "get:Delete"),
 
-		//权限管理
+		// 权限管理
 		beego.NSRouter("/auth", &backend.AuthController{}),
 		beego.NSRouter("/auth/add", &backend.AuthController{}, "get:Add"),
 		beego.NSRouter("/auth/edit", &backend.AuthController{}, "get:Edit"),
 		beego.NSRouter("/auth/goadd", &backend.AuthController{}, "post:GoAdd"),
 		beego.NSRouter("/auth/goedit", &backend.AuthController{}, "post:GoEdit"),
 		beego.NSRouter("/auth/delete", &backend.AuthController{}, "get:Delete"),
+
+		// 轮播图管理
+		beego.NSRouter("/banner", &backend.BannerController{}),
+		beego.NSRouter("/banner/add", &backend.BannerController{}, "get:Add"),
+		beego.NSRouter("/banner/edit", &backend.BannerController{}, "get:Edit"),
+		beego.NSRouter("/banner/goadd", &backend.BannerController{}, "post:GoAdd"),
+		beego.NSRouter("/banner/goedit", &backend.BannerController{}, "post:GoEdit"),
+		beego.NSRouter("/banner/delete", &backend.BannerController{}, "get:Delete"),
 	)
 	beego.AddNamespace(ns)
 }
